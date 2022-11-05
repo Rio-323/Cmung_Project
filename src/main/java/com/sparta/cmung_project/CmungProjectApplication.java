878a544c9e16ee6cdd5fccebe5dad01433a6730b
Rio-323 <kimsiyeon5214@gmail.com>
2022-11-05 21:09:29 +0900

    초기 세팅 설정

---
+++ src/main/java/com/sparta/cmung_project/CmungProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.cmung_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmungProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run ( CmungProjectApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/sparta/cmung_project/CmungProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.cmung_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmungProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
