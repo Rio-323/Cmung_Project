8a51f143d06ae3a17c7a66c7acf42f6fbdc52a72
Rio-323 <kimsiyeon5214@gmail.com>
2022-11-09 22:59:07 +0900

    어노테이션 제거

--- src/main/java/com/sparta/cmung_project/dto/AllPostResponseDto.java
+++ src/main/java/com/sparta/cmung_project/dto/AllPostResponseDto.java
@@ -13,7 +13,6 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@RequiredArgsConstructor
 public class AllPostResponseDto {
     private Long id;
     private String nickname;
