02f9b5fa199f86bb2c2f33f8cd0156af7dc1dda4
Rio-323 <kimsiyeon5214@gmail.com>
2022-11-09 16:53:43 +0900

    search기능 PostService구현 이후 구현 예정

--- src/main/java/com/sparta/cmung_project/dto/MemberReqDto.java
+++ src/main/java/com/sparta/cmung_project/dto/MemberReqDto.java
@@ -13,7 +13,7 @@ public class MemberReqDto {
 
     @NotBlank(message = "아이디는 반드시 입력해야합니다")
     @Size(min = 4, max = 12, message = "아이디의 길이는 4 ~ 12입니다.")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)[a-z\\d]{4,12}$", message = "아이디는 영문 소문자와 숫자만 입력가능 합니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)[a-z\\d]{4,12}$", message = "아이디는 영문과 소문자, 숫자 모두 입력하여야 합니다.")
     private String userId;
 
     @NotBlank(message = "비밀번호는 반드시 입력해야합니다.")
