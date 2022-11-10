18eb58b7567dff0071cf03e072278143cafb8d16
Rio-323 <kimsiyeon5214@gmail.com>
2022-11-10 21:27:16 +0900

    Nickname 정규식 표현 + 예외처리 추가

--- src/main/java/com/sparta/cmung_project/service/MemberService.java
+++ src/main/java/com/sparta/cmung_project/service/MemberService.java
@@ -11,7 +11,6 @@ import com.sparta.cmung_project.model.RefreshToken;
 import com.sparta.cmung_project.repository.MemberRepository;
 import com.sparta.cmung_project.repository.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
