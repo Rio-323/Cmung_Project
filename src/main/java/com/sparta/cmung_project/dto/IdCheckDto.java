fe12685f35be75ebf51c33ad87f3077daa3d1e10
Rio-323 <kimsiyeon5214@gmail.com>
2022-11-11 12:02:16 +0900

    아이디 정규식 크기 조정

--- src/main/java/com/sparta/cmung_project/dto/IdCheckDto.java
+++ src/main/java/com/sparta/cmung_project/dto/IdCheckDto.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.Size;
 public class IdCheckDto {
 
     @NotBlank(message = "아이디는 반드시 입력해야합니다")
-    @Size(min = 4, max = 12, message = "아이디의 길이는 4~12입니다.")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)[a-z\\d]{4,12}$", message = "아이디는 소문자와 숫자만 포함해야합니다.")
+    @Size(min = 4, max = 20, message = "아이디의 길이는 4~20입니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)[a-z\\d]{4,20}$", message = "아이디는 소문자와 숫자만 포함해야합니다.")
     private String userId;
 }
