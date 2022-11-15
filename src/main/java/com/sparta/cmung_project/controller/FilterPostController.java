79e476cb92093d1382d1fc2d35e26ec28789f7a5
wepungsan <wepungsan@gmail.com>
2022-11-15 14:06:09 +0900

    complete log.info

--- src/main/java/com/sparta/cmung_project/controller/FilterPostController.java
+++ src/main/java/com/sparta/cmung_project/controller/FilterPostController.java
@@ -3,11 +3,13 @@ package com.sparta.cmung_project.controller;
 import com.sparta.cmung_project.dto.GlobalResDto;
 import com.sparta.cmung_project.service.PostService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/filter")
@@ -16,6 +18,8 @@ public class FilterPostController {
 
     @GetMapping
     public GlobalResDto<?> filterPost(@RequestParam(name = "category") String category) {
+        log.info("게시글 필터");
+        
         return postService.filterPost ( category );
     }
 }
--- src/main/java/com/sparta/cmung_project/controller/UserController.java
+++ src/main/java/com/sparta/cmung_project/controller/UserController.java
@@ -5,9 +5,11 @@ import com.sparta.cmung_project.exception.CustomException;
 import com.sparta.cmung_project.security.user.UserDetailsImpl;
 import com.sparta.cmung_project.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RequiredArgsConstructor // 기본 생성자를 만들어줍니다.
 @RestController // JSON으로 데이터를 주고받음을 선언합니다.
 @RequestMapping("/api")
@@ -17,18 +19,24 @@ public class UserController {
     // 회원 정보 조회
     @GetMapping("/users/{userId}")
     public GlobalResDto<?> getUserInfo(@PathVariable Long userId) throws CustomException {
+        log.info("다른 회원 정보 조회");
+        
         return userService.getUserInfo(userId);
     }
     
     // 회원 반려동물 조회
     @GetMapping("/users/{userId}/pet")
     public GlobalResDto<?> getUserPetInfo(@PathVariable Long userId) throws CustomException {
+        log.info("다른 회원 애완동물 조회");
+
         return userService.getUserPetInfo(userId);
     }
 
     // 회원 게시글 조회
     @GetMapping("/users/{userId}/posts")
     public GlobalResDto<?> getUserPosts(@PathVariable Long userId) throws RuntimeException {
+        log.info("다른 회원 게시글 조회");
+
         return userService.getUserPosts(userId);
     }
 }
