a54f429d24424be674508d9e25f4bca11046b667
Merong <112916559+meruberu@users.noreply.github.com>
2022-12-16 11:42:56 +0900

    Update WebSocketConfig.java

--- src/main/java/com/sparta/cmung_project/websocket/WebSocketConfig.java
+++ src/main/java/com/sparta/cmung_project/websocket/WebSocketConfig.java
@@ -23,6 +23,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws/chat")//SockJS 연결 주소
                 .setAllowedOriginPatterns("*")
                 .withSockJS(); //버전 낮은 브라우저에서도 적용 가능
-        // 주소 : ws://localhost:8080/ws-stomp
     }
 }
